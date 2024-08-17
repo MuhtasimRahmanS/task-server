51e7d047fe870a5ce686e2cf65e1ad65641ded12
MuhtasimRahmanS <muhtasimrahman01@gmail.com>
2024-08-17 19:03:49 +0600

    finish

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://jobtaskscic.netlify.app/"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qov5o8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,22 +27,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect(); //comment
+    // await client.connect();
 
     const productCollection = client.db("task").collection("allProduct");
 
-    app.get("/allProduct", async (req, res) => {
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
+    app.get("/products", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const filter = req.query.filter;
+      const sort = req.query.sort;
+      const search = req.query.search;
+      console.log(size, page);
+
+      let query = {
+        ProductName: { $regex: search, $options: "i" },
+      };
+      if (filter) query.Category = filter;
+      let options = {};
+      if (sort)
+        options = {
+          sort: {
+            Price: sort === "asc" ? 1 : -1,
+          },
+        };
+      const result = await productCollection
+        .find(query, options)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
       res.send(result);
     });
 
+    app.get("/product-count", async (req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
+      let query = {
+        ProductName: { $regex: search, $options: "i" },
+      };
+      if (filter) query.category = filter;
+      const count = await productCollection.countDocuments(query);
+
+      res.send({ count });
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-    //
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
