0c579aabb51af75d0464cbfe3a30e0cc6ff676b7
MuhtasimRahmanS <muhtasimrahman01@gmail.com>
2024-08-18 16:14:10 +0600

    solve

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware Setup
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://jobtaskscic.netlify.app/"],
+    origin: ["http://localhost:5173", "https://jobtaskscic.netlify.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
